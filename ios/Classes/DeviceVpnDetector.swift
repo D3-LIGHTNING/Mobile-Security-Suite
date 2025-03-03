0e34debe564d608268d2ab3d023255c83e96d2b5
Pradhu <vidhuramachandran1@gmail.com>
2025-03-03 12:54:00 +0530

    IOS basic plugin pattern implementation and VPN detection

---
+++ ios/Classes/DeviceVpnDetector.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Flutter
+
+public class DeviceVpnDetector : MssPluginBindinginterface {
+    func performActivity(arguments: MssArguments, result: @escaping FlutterResult) {
+        result(detectVpnActivity())
+    }
+    
+    private func detectVpnActivity() -> Bool {
+        let vpnProtocolKeyIdentifiers = ["tap", "tun", "ppp", "ipsec", "utun", "pptp"]
+        
+        guard let cfDict = CFNetworkCopySystemProxySettings() else { return false }
+        let nsDict = cfDict.takeRetainedValue() as NSDictionary
+        
+        guard let keys = nsDict["__SCOPED__"] as? NSDictionary,
+              let allKeys = keys.allKeys as? [String] else { return false }
+        
+        for key in allKeys {
+            for protocolId in vpnProtocolKeyIdentifiers
+            where key.starts(with: protocolId) { return true }
+        }
+        
+        return false
+        
+    }
+    
+}
--- ios/Classes/MobileSecuritySuitePlugin.swift
+++ ios/Classes/MobileSecuritySuitePlugin.swift
@@ -1,19 +1,34 @@
 import Flutter
-import UIKit
 
 public class MobileSecuritySuitePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "mobile_security_suite", binaryMessenger: registrar.messenger())
-    let instance = MobileSecuritySuitePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    var deviceVpnDetector: DeviceVpnDetector?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "mobile_security_suite", binaryMessenger: registrar.messenger())
+        let instance = MobileSecuritySuitePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+    }
+}
+
+protocol MssPluginBindinginterface {
+    func performActivity(arguments: MssArguments, result: @escaping FlutterResult)
+}
+
+public class MssArguments {
+    let arguments: [String: Any]
+    init(arguments: [String : Any]) { self.arguments = arguments }
+}
+
+public class MethodNames {
+    static let VPN = "vpn"
+    static let PROXY = "proxy"
+    static let WIFI_SECURITY = "wifiSecurity"
+    static let SCREEN_MIRRORING = "screenMirroring"
+    static let LOCATION_MOCK = "locationMock"
+    static let TAP_JACKING = "tapJacking"
+    static let SCREEN_OBFUSCATE = "screenObfuscate"
 }
